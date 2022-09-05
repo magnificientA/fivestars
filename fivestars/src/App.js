9bb03d4ae0aade2870dfdfeb0b3598195332c3c7
magnificientA <abdulalimzakariyah@gmail.com>
2022-09-05 17:03:46 +0100

    first commit

---
+++ fivestars/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App ">
+        <div class="motion">
+                <span>WELCOME  TO FIVE STARS MARKETING MEDIA</span>
+            </div>
+            <div class="motion motion2">
+                <span>WELCOME  TO FIVE STARS MARKETING MEDIA</span>
+            </div>
+    </div>
+  );
+}
+
+export default App;
